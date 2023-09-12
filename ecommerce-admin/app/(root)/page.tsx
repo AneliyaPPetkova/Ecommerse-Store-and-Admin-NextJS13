01c72fae5d7e8e5f2a882c296bcdb2c8caea77b4
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-09-12 13:09:30 +0300

    Add the Button component from ShadCn.

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p>Ecommerce Admin Dashboard</p>
-  )
+    <>
+      <p>Ecommerce Admin Dashboard</p>
+      <div className="p-5">
+        <Button size={"lg"} variant={"secondary"}>
+          Login
+        </Button>
+      </div>
+    </>
+  );
 }
