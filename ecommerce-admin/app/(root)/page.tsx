3936990acc82498fc1735747e7ae9867b00ddf1b
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-09-13 23:06:49 +0300

    Integrate Clerk for User Authentication and Profile management.

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <Button size={"lg"} variant={"secondary"}>
           Login
         </Button>
+        <UserButton afterSignOutUrl="/"/>
       </div>
     </>
   );
